45fe8ad23ee600cdce7322c3dc94b57ba510cc20
James FE <jamesfrontend@gmail.com>
2022-03-23 12:13:59 -0400

    feat: meta image

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,10 +4,8 @@ import * as React from 'react';
 import { MainHeader } from '../src/components/mainHeader';
 import { useRouter } from 'next/dist/client/router';
 
-
 const MyApp = ({ Component, pageProps }) => {
     const router = useRouter();
-
 
     const routes = [
         {
@@ -31,11 +29,28 @@ const MyApp = ({ Component, pageProps }) => {
                     rel='stylesheet'
                 />
                 <link rel='manifest' href='site.webmanifest' />
-                <meta name='description' content='Design System React Demo' />
+                <meta name='description' content='Designer web tools' />
                 <meta
                     name='viewport'
                     content='initial-scale=1.0, width=device-width'
                 />
+                <meta
+                    name='title'
+                    property='og:title'
+                    content='Designer web tools'
+                />
+                <meta property='og:type' content='Website' />
+                <meta
+                    name='image'
+                    property='og:image'
+                    content='https://jamesfrontend.vercel.app/int-tool.jpg'
+                />
+                <meta
+                    name='description'
+                    property='og:description'
+                    content='Designer web tools'
+                />
+                <meta name='author' content='James Nguyen' />
             </Head>
             <div className='msk-galaxy'>
                 <div className='msk-world'>
